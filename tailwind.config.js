5b06e142c12170ba626ddc93fa6259db66667ff3
Marco Aurelio Rezende <marco.aurelio.rsilva7@gmail.com>
2025-07-16 10:13:33 -0300

    chore: add poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [".index/html", "./src/**/*.{js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
