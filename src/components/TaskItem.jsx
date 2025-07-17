e0e040e130252ca85681d8b69a08c10e3bc833c0
Marco Aurelio Rezende <marco.aurelio.rsilva7@gmail.com>
2025-07-17 13:49:59 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,24 +1,48 @@
+import CheckIcon from "../assets/icons/check.svg?react"
+import DetailsIcon from "../assets/icons/details.svg?react"
+import LoaderIcon from "../assets/icons/loader.svg?react"
+
 const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
     if (task.status === "done") {
       // aqui eu deixo os estilos que são específicos em cada status
-      return "bg-[#00ADB5] text-[#00ADB5] bg-opacity-10 "
+      return "bg-[#00ADB5] text-[#00ADB5]  "
     }
 
     if (task.status === "in_progress") {
-      return "bg-[#FFAA04] text-[#FFAA04] bg-opacity-10 "
+      return "bg-[#FFAA04] text-[#FFAA04]  "
     }
 
     if (task.status === "not_started") {
-      return "bg-[#35383E] text-[#35383E] bg-opacity-10"
+      return "bg-[#35383E] bg-opacity-10 text-[#35383E] "
     }
   }
 
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`} // aqui eu deixo os estilos que são comuns em todos os itens
+      className={`flex items-center justify-between gap-2 rounded-lg bg-opacity-10 px-4 py-3 text-sm ${getStatusClasses()}`} // aqui eu deixo os estilos que são comuns em todos os itens
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg ${getStatusClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === "done"}
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === "done" && <CheckIcon />}
+          {task.status === "in_progress" && (
+            <LoaderIcon className="animate-spin" />
+          )}
+        </label>
+
+        {task.title}
+      </div>
+
+      <a href="#" className="transition hover:opacity-75">
+        <DetailsIcon />
+      </a>
     </div>
   )
 }
