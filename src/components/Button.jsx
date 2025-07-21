ed84e944373b67c86ef8d6193cba3c13bb44ea66
Marco Aurelio Rezende <marco.aurelio.rsilva7@gmail.com>
2025-07-21 14:39:11 -0300

    chore: add tailwind-variant

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,41 +1,51 @@
+import PropTypes from "prop-types"
+import { tv } from "tailwind-variants"
+
 const Button = ({
   children,
-  variant = "primary",
+  color = "primary",
   size = "small",
   className,
   ...rest
 }) => {
-  const getVariantClasses = () => {
-    if (variant === "primary") {
-      return "bg-brand-primary text-white"
-    }
+  const button = tv({
+    base: `flex items-center justify-center gap-2 rounded-md px-3 font-semibold transition hover:opacity-75`,
+    variants: {
+      color: {
+        primary: "bg-brand-primary text-white",
+        ghost: "bg-transparent text-brand-dark-gray",
+        secondary: "bg-brand-light-gray text-brand-dark-blue",
+        danger: "bg-brand-danger text-brand-white",
+      },
+      size: {
+        small: "py-1 text-xs",
+        large: "py-2 text-sm",
+      },
+      disabled: {
+        true: "cursor-not-allowed opacity-50 hover:opacity-50",
+      },
+    },
+    defaultVariants: {
+      color: "primary",
+      size: "small",
+    },
+  })
 
-    if (variant === "secondary") {
-      return "bg-brand-light-gray text-brand-dark-blue"
-    }
-
-    if (variant === "ghost") {
-      return "bg-transparent text-brand-dark-gray"
-    }
-  }
-
-  const getSizeClasses = () => {
-    if (size === "small") {
-      return "py-1 text-sx"
-    }
-
-    if (size === "large") {
-      return "py-2 text-sm"
-    }
-  }
   return (
     <button
-      className={`flex items-center justify-center gap-1 rounded-md px-3 py-1 text-xs font-semibold transition hover:opacity-75 ${getVariantClasses()} ${getSizeClasses()} ${className}`}
+      className={button({ color, size, disabled: rest.disabled, className })}
       {...rest}
     >
       {children}
     </button>
   )
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  color: PropTypes.oneOf(["primary", "ghost", "secondary", "danger"]),
+  size: PropTypes.oneOf(["small", "large"]),
+  className: PropTypes.string,
 }
 
 export default Button
